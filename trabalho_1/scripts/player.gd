ef0570c78a66df0da85528feb71b1440268585f1
Daniel <danielmrezende@yahoo.com.br>
2025-11-13 15:40:16 -0300

    Starting player implementation;

---
+++ trabalho_1/scripts/player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed : float
+
+var screen : Rect2
+var ground : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dx = Input.get_axis("ui_left", "ui_right")
+	
+	position.x += dx * speed * delta
